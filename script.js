e8afdbc33247b88942b11c54f7e896a5ec6df2e8
darmijo15 <daniel.armijo@wsu.edu>
2022-03-18 21:26:05 -0700

    Add click eventListener to the change-color button that sets the body backgroundColor to a random rgb value

---
+++ script.js
@@ -0,0 +1,9 @@
+const colorButton = document.querySelector("#change-color");
+
+colorButton.addEventListener('click', function() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+})
